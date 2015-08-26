21b042891b0312d9725f8644689542b30c47da2c
thyrons <willynarvaez6@gmail.com>
2015-08-26 15:55:55 -0500

    cambios
    cambios

--- procesos/base.php
+++ procesos/base.php
@@ -1,13 +1,8 @@
 <?php
 
 function conectarse() {
-<<<<<<< HEAD
     if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=catalogo user=postgres password=rootdow"))) {
     	exit();        
-=======
-    if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=ventas_catalogo user=postgres password=sisweb"))) {
-        exit();
->>>>>>> origin/master
     } else {
         
     }
